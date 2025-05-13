96e3982fae10d55b1b253e8de21f9c72d3084d14
EL.MLIKI.HICHAM <elmliki.hicham@gmail.com>
2025-05-13 14:27:38 +0100

    config server

--- server.js
+++ server.js
@@ -1,2 +1,14 @@
-const app = require('./app'); // This imports the app.js file
-module.exports = app;
+// server.js
+require('dotenv').config();
+const http = require('http');
+const app = require('./app');
+
+// Get port from environment or default to 3030
+const PORT = process.env.PORT || 3030;
+
+// Create and start the server
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`ðŸš€ Server is running on port ${PORT}`);
+});
